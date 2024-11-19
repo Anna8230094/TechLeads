448a223cca8f6df246038b11cc377ec2790e5efa
t8230136 <t8230136@aueb.gr>
2024-11-19 19:45:43 +0200

    Δημιουργία κλάσης HomeController

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("hireandgo/home")
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeControl(Model model) {
+        model.addAttribute("message", "Welcome to Hire&Go!");
+        return "home";
+    }
+
+}
