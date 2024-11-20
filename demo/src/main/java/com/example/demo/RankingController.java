e8b04c7f8f3557ccb619f02b4cf8c05093d338f7
t8230136 <t8230136@aueb.gr>
2024-11-20 13:33:47 +0200

    Δημιουργία της κλάσης RankingController

--- demo/src/main/java/com/example/demo/RankingController.java
+++ demo/src/main/java/com/example/demo/RankingController.java
@@ -6,13 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/hireandgo/config")
-public class ConfigurationController {
-    
-    @GetMapping("/")
-    public String configControl(Model model) {
-        model.addAttribute("message", "Please enter your name");
-        return "config";
-    }
+@RequestMapping("/hireandgo/ranking")
+public class RankingController {
 
+    @GetMapping("/")
+    public String rankingControl(Model model) {
+        model.addAttribute("message", "Please enter your email");
+        return "ranking";
+    }
+    
 }
