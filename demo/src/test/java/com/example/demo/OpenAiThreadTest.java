9975cfe566e367999bc1a98f6a22820a5155ed24
Anna Megalou <annamegalou3@gmail.com>
2024-11-21 22:01:29 +0200

    Some changes

--- demo/src/test/java/com/example/demo/OpenAiThreadTest.java
+++ demo/src/test/java/com/example/demo/OpenAiThreadTest.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.IOException;
 
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.example.demo.openai.OpenAiAssistant;
+import com.example.demo.openai.OpenAiThread;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
