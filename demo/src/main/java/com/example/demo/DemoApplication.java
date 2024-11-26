6a53c5439c8969d06416b75466217401baaf8f46
Anna Megalou <annamegalou3@gmail.com>
2024-11-26 22:56:33 +0200

    some changes

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,21 +9,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import com.example.demo.mail.EmailService;
-import com.example.demo.openai.agents.Extractor;
-import com.example.demo.openai.threads.ExtractorThread;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
 
-	@Override
+	/*@Override
 	public void run(String... args) throws Exception {
 
-		/*String messageRegiser = "Here are the details provided by the user:Industry: Tech,Role: Software Engineer,Proficiency Level: Mid-Level,Related Qualification: Python";
+		String messageRegiser = "Here are the details provided by the user:Industry: Tech,Role: Software Engineer,Proficiency Level: Mid-Level,Related Qualification: Python";
 		Register register = new Register(Register.MODEL, Register.INSTRUCTIONS, Register.NAME);
 
 		OpenAiThread openAiThread = new OpenAiThread(messageRegiser, Register.INSTRUCTIONS, register.getAssistantId());
@@ -32,7 +30,7 @@ public class DemoApplication implements CommandLineRunner {
 		openAiThread.addMessage();
 		openAiThread.run();
 		String registerResponse = openAiThread.getRequest();
-		System.out.println(registerResponse);*/
+		System.out.println(registerResponse);
 
 		Extractor extractor = new Extractor(Extractor.MODEL, Extractor.INSTRUCTIONS, Extractor.NAME);
 
@@ -46,7 +44,7 @@ public class DemoApplication implements CommandLineRunner {
 		extractorThread.getRequest();
 
 
-	}
+	}*/
 
 	@Bean
     @SuppressWarnings("ConvertToTryWithResources")
@@ -61,7 +59,7 @@ public class DemoApplication implements CommandLineRunner {
 			System.out.println("Enter the body of the email:");
 			String body = scanner.nextLine();
 
-			String to = "aggmegalou@gmail.com ";
+			String to = "annamegalou3@gmail.com ";
 			emailService.sendEmail(to, subject, body);
 
 			System.out.println("Email sent successfully to " + to + "!");
