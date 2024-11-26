b94bedbcf4c17194e438003b195ff68b20ff1094
Konstantia Stergiou <t8230144@aueb.gr>
2024-11-26 16:55:44 +0200

    some changes

--- demo/src/main/java/com/example/demo/database/ApplicantsComponent.java
+++ demo/src/main/java/com/example/demo/database/ApplicantsComponent.java
@@ -15,10 +15,23 @@ public class ApplicantsComponent implements CommandLineRunner{
 	public void run(String... args) {
 
 		Applicants applicant = new Applicants();
-		applicant.setFirstName("OLY");
-		applicant.setLastName("MEGALOU");
+		applicant.setFirstName("Olibia");
+		applicant.setFirstName("Maria");
+		applicant.setFirstName("Alex");
+		applicant.setFirstName("Andrew");
+		applicant.setLastName("Megalou");
+		applicant.setLastName("Iliadi");
+		applicant.setLastName("Papadopoulos");
+		applicant.setLastName("Papandreou");
 		applicant.setEmail("oly.meg@example.com");
+		applicant.setEmail("maria.ili@example.com");
+		applicant.setEmail("alex.pap@example.com");
+		applicant.setEmail("an.papandreou@example.com");
 		applicant.setIndustry("Finance");
+		applicant.setIndustry("Human Resources");
+		applicant.setIndustry("Marketing and Communication");
+		applicant.setIndustry("Architecture");
+
 		applicantsService.saveApplicants(applicant);
 
 	}
