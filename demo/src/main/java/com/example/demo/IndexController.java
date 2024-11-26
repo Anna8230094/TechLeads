953408d4722b10b5c441647f7e09d2a2c1cb0e30
t8230136 <t8230136@aueb.gr>
2024-11-26 13:52:20 +0200

    Δημιουργία της κλάσης IndexController

--- demo/src/main/java/com/example/demo/IndexController.java
+++ demo/src/main/java/com/example/demo/IndexController.java
@@ -2,17 +2,15 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/hireandgo/home")
-public class HomeController {
+@RequestMapping("/hireandgo/index")
+public class IndexController {
 
     @GetMapping("/")
-    public String homeControl(Model model) {
-        model.addAttribute("message", "Welcome to Hire&Go!");
-        return "home";
+    public String indexControl() {
+        return "index";
     }
 
 }
