21a65407818ff5809792e07ecd55c77ace4d1eaf
Anna Megalou <annamegalou3@gmail.com>
2024-11-30 13:56:57 +0200

    A changed in order to load the html of nelly

--- demo/src/main/java/com/example/demo/RegistrationFormController.java
+++ demo/src/main/java/com/example/demo/RegistrationFormController.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hireandgo/registrationform")
+@RequestMapping("/hireandgo/home/")
 public class RegistrationFormController {
     
-    @GetMapping("/")
+    @GetMapping("/registrationform.html")
     public String registrationControl() {
         return "registrationform";
     }
