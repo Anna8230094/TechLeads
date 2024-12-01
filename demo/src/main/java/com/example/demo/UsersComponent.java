f477ca36cef2fa5948258fa1e2bde9cc4ecbc252
Konstantia Stergiou <t8230144@aueb.gr>
2024-12-02 01:06:09 +0200

    CHANGED  OBJECTS

--- demo/src/main/java/com/example/demo/UsersComponent.java
+++ demo/src/main/java/com/example/demo/UsersComponent.java
@@ -14,25 +14,34 @@ public class UsersComponent implements CommandLineRunner{
     @Override
 	public void run(String... args) {
 
-		Users user = new Users();
-		user.setFirstName("Olibia");
-		user.setFirstName("Maria");
-		user.setFirstName("Alex");
-		user.setFirstName("Andrew");
-		user.setLastName("Megalou");
-		user.setLastName("Iliadi");
-		user.setLastName("Papadopoulos");
-		user.setLastName("Papandreou");
-		user.setEmail("oly.meg@example.com");
-		user.setEmail("maria.ili@example.com");
-		user.setEmail("alex.pap@example.com");
-		user.setEmail("an.papandreou@example.com");
-		user.setIndustry("Finance");
-		user.setIndustry("Human Resources");
-		user.setIndustry("Marketing and Communication");
-		user.setIndustry("Architecture");
+    Users user1 = new Users();
+    user1.setFirstName("Olibia");
+    user1.setLastName("Megalou");
+    user1.setEmail("oly.meg@example.com");
+    user1.setIndustry("Finance");
 
-		usersService.saveUsers(user);
+    Users user2 = new Users();
+    user2.setFirstName("Maria");
+    user2.setLastName("Iliadi");
+    user2.setEmail("maria.ili@example.com");
+    user2.setIndustry("Human Resources");
 
-	}
+    Users user3 = new Users();
+    user3.setFirstName("Alex");
+    user3.setLastName("Papadopoulos");
+    user3.setEmail("alex.pap@example.com");
+    user3.setIndustry("Marketing and Communication");
+
+    Users user4 = new Users();
+    user4.setFirstName("Andrew");
+    user4.setLastName("Papandreou");
+    user4.setEmail("an.papandreou@example.com");
+    user4.setIndustry("Architecture");
+
+    usersService.saveUsers(user1);
+    usersService.saveUsers(user2);
+    usersService.saveUsers(user3);
+    usersService.saveUsers(user4);
+}
+
 } 
