8c9fa9af6dc5ffe8ee79e4e9c6e0750479dfa419
Anna Megalou <annamegalou3@gmail.com>
2024-12-01 23:14:02 +0200

    Development is in the same stage with main

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.mail.EmailService;
+import com.example.demo.openai.agents.Register;
+import com.example.demo.openai.threads.OpenAiThread;
 
 @SpringBootApplication
-public class DemoApplication {//implements CommandLineRunner 
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
-/*
+
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -24,7 +33,7 @@ public class DemoApplication {//implements CommandLineRunner
 		openAiThread.run();
 		String registerResponse = openAiThread.getRequest();
 		System.out.println(registerResponse);
-
+		/* 
 		Extractor extractor = new Extractor(Extractor.MODEL, Extractor.INSTRUCTIONS, Extractor.NAME);
 
 		String messageExtractor = "Convert to me  the pdf in csv format not a tablr or file";
@@ -34,8 +43,7 @@ public class DemoApplication {//implements CommandLineRunner
 		extractorThread.createThread();
 		extractorThread.addMessage();
 		extractorThread.run();
-		extractorThread.getRequest();
-
+		extractorThread.getRequest();*/
 
 	}
 
@@ -60,5 +68,6 @@ public class DemoApplication {//implements CommandLineRunner
 		};
 
 	}
-*/
+	
+
 }
