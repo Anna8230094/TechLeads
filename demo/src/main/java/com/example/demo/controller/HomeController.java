f70f9f2f5365edb94ba9025607ed6a9cf7624af4
Anna Megalou <annamegalou3@gmail.com>
2024-12-01 20:48:53 +0200

    Firs goal of main is completed

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,8 @@
-package com.example.demo;
+package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/hireandgo/home")
