7b1a5ae931d5f6f712b79ba6dc046ec678332ad3
Anna Megalou <annamegalou3@gmail.com>
2024-12-05 17:59:26 +0200

    I merged valerias branch

--- demo/src/main/java/com/example/demo/openai/agents/ReviewerResearcher.java
+++ demo/src/main/java/com/example/demo/openai/agents/ReviewerResearcher.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 public class ReviewerResearcher extends OpenAiAssistant {
 
     public static final String INSTRUCTIONS = "You are responsible for a cv ranking procedure where other agents are part of as well."
-    + "Your role is to receive the Researcher’s result and review it as well as estimate the results and decide whether or not"
-    + "they are correct or they need further corrections. 1. Check if Researcher’s results are correct."
-    + "If they are, return “they are correct”. 2. If results are not correct, suggest corrections. ";
+            + "Your role is to receive the Researcher’s result and review it as well as estimate the results and decide whether or not"
+            + "they are correct or they need further corrections. 1. Check if Researcher’s results are correct."
+            + "If they are, return “they are correct”. 2. If results are not correct, suggest corrections. ";
+
     public static final String MODEL = "gpt-4o-mini";
     public static final String NAME = "ReviewerResearcher";
 
