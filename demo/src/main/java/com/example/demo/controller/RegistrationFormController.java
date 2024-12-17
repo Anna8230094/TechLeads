e7542c6fa35f5148ce57e9971955e6767a59a821
t8230136 <t8230136@aueb.gr>
2024-12-18 00:34:11 +0200

    Some changes

--- demo/src/main/java/com/example/demo/controller/RegistrationFormController.java
+++ demo/src/main/java/com/example/demo/controller/RegistrationFormController.java
@@ -1,12 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.database.reasearcher.ResearcherService;
+import com.example.demo.database.user.UsersService;
+
 @Controller
 @RequestMapping("/hireandgo/home/")
 public class RegistrationFormController {
+    @Autowired
+   UsersService usersService;
+
+
+   @Autowired
+    ResearcherService researcerService;
     
     @GetMapping("/registrationform.html")
     public String registrationControl() {
