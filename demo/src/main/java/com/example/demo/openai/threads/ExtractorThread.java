41df4801675bd941db1e7bb991c4b42f659b2b63
aggmegalou <t8230093@aueb.gr>
2025-01-03 21:08:50 +0200

    a chane

--- demo/src/main/java/com/example/demo/openai/threads/ExtractorThread.java
+++ demo/src/main/java/com/example/demo/openai/threads/ExtractorThread.java
@@ -17,20 +17,13 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-/**
- * This class represents my class in Java.
- * 
- * @author Anna Maria Megalou
- * @version 1.0
- */
-
 @Service("ExtractorThread")
 public class ExtractorThread extends OpenAiThread {
 
     private String fileId = "";
 
     public CompletableFuture<String> uploadFile(MultipartFile multipartFile) throws IOException {
-
+        
         OkHttpClient client = new OkHttpClient().newBuilder().connectTimeout(30, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS).build();
