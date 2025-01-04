bb7aee4b0d7d4064f220b047c3ceb0235df6d3f5
aggmegalou <t8230093@aueb.gr>
2025-01-04 21:31:26 +0200

    changes in order to run the programm

--- demo/src/main/java/com/example/demo/controller/RankingController.java
+++ demo/src/main/java/com/example/demo/controller/RankingController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/hireandgo/ranking")
+@RequestMapping("/hireandgo/resultspage")
 public class RankingController {
 
     @GetMapping("/")
     public String rankingControl(Model model) {
-        model.addAttribute("message", "Please enter your email");
-        return "ranking";
+        return "resultspage";
     }
     
 }
