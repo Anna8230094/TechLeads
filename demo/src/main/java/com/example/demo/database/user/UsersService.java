afcde4d45e4ed8dd4d0770a1b9e846df15c901f7
Konstantia Stergiou <t8230144@aueb.gr>
2025-01-08 21:14:50 +0200

    changes in service users

--- demo/src/main/java/com/example/demo/database/user/UsersService.java
+++ demo/src/main/java/com/example/demo/database/user/UsersService.java
@@ -8,19 +8,26 @@
  * @version 1.0
  */
 
-package com.example.demo.database.user;
+ package com.example.demo.database.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UsersService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public Users saveUsers(Users users) {
-        return userRepository.save(users);
-    }
-} 
-
+ import java.util.concurrent.CompletableFuture;
+ 
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.scheduling.annotation.Async;
+ import org.springframework.stereotype.Service;
+ 
+ @Service
+ public class UsersService {
+ 
+     @Autowired
+     private UserRepository userRepository;
+ 
+     @Async
+     public CompletableFuture<Void> saveUsers(Users users) {
+         userRepository.save(users);
+         System.out.println("The user is saved");
+         return CompletableFuture.completedFuture(null);
+     }
+ 
+ }
+ 
