aeec069bd37b406926e47ecb72a9e0f88a0a1933
Anna Megalou <annamegalou3@gmail.com>
2025-01-12 16:48:57 +0200

    fic:change env p[roperties path

--- demo/src/main/java/com/example/demo/controller/SuccesControler.java
+++ demo/src/main/java/com/example/demo/controller/SuccesControler.java
@@ -1,18 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/hireandgo/home/registrationform/")
 public class SuccesControler {
 
-    @GetMapping("/success/{id}")
-    public String handleSucces(Model model, @RequestParam String username, @PathVariable(value = "id") Long id) {
+    @GetMapping("/success")
+    public String handleSucces() {
         return "success";
     }
 
