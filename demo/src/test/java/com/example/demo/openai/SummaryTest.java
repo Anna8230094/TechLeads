dc3b096294b66eb88421fa15f6afcbe374804797
Nellyandr <t8230007@aueb.gr>
2025-01-18 18:18:48 +0200

    Made Test for Summary

---
+++ demo/src/test/java/com/example/demo/openai/SummaryTest.java
@@ -0,0 +1,157 @@
+package com.example.demo.openai;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import com.example.demo.openai.agents.OpenAiAssistant;
+import com.example.demo.openai.agents.Register;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * This class represents my class in Java.
+ * 
+ * @author Nektaria-Evangelia Androulaki
+ * @version 1.0
+ */
+
+@ExtendWith(SpringExtension.class)
+@TestInstance(Lifecycle.PER_CLASS)
+public class SummaryTest {
+
+    @Spy
+    @InjectMocks
+    private SummaryTest summary;
+
+    @BeforeAll
+    void setUp() throws IOException {
+        ReflectionTestUtils.setField(summary, "instructions", SummaryTest.INSTRUCTIONS);
+        ReflectionTestUtils.setField(summary, "name", SummaryTest.NAME);
+        ReflectionTestUtils.setField(summary, "model", SummaryTest.MODEL);
+    }
+
+    private static void mockHttpClient(OpenAiAssistant runtimeClass, final String serializedBody) throws IOException {
+        Response response = new Response.Builder()
+                .request(new Request.Builder().url("http://url.com").build())
+                .protocol(Protocol.HTTP_1_1)
+                .code(200).message("").body(
+                        ResponseBody.create(
+                                serializedBody,
+                                MediaType.parse("application/json")))
+                .build();
+        doReturn(CompletableFuture.completedFuture(response)).when(runtimeClass).sendRequest(anyString(), anyString());
+    }
+
+    @Test
+    void createAssistantTest() throws IOException, InterruptedException, ExecutionException {
+
+        mockHttpClient(summary, "{\"id\": \"assistant-id-123\"}");
+
+        assertEquals(summary.getModel(), SummaryTest.MODEL);
+        assertEquals(summary.getInstructions(), SummaryTest.INSTRUCTIONS);
+        assertEquals(summary.getName(), SummaryTest.NAME);
+        CompletableFuture<String> future = summary.createAiAssistant();
+        future.join();
+        assertEquals("assistant-id-123", future.get());
+    }
+
+    @Test
+    void testCreateAiAssistant() throws IOException, JSONException, InterruptedException, ExecutionException {
+        // Simulate successful response
+        // when(mockResponse.isSuccessful()).thenReturn(true);
+        String mockResponseBody = "{\"id\":\"assistant-id-123\"}";
+        mockHttpClient(summary, mockResponseBody);
+
+        assertEquals(summary.loadKey(), "${OPENAI_API_KEY}");
+        assertEquals(summary.getName(), "Register");
+        CompletableFuture<String> response = summary.createAiAssistant();
+        assertEquals("assistant-id-123", response.join());
+    }
+
+    @Test
+    void loadKeyTest() {
+        assertNotNull(summary.loadKey(), "The key must not be null");
+    }
+
+    @Test
+    void testBuildJsonForAssistant() {
+        // Generate JSON and verify its structure
+        String json = summary.buildJsonForAssistant();
+        JSONObject jsonObject = new JSONObject(json);
+
+        assertEquals("gpt-4o-mini", jsonObject.getString("model"));
+        assertEquals(
+                "You are responsible for a procedure of cv ranking where other agents are part of as well. Your role is to receive a job description and turn it in csv format (return it in text form)",
+                jsonObject.getString("instructions"));
+        assertEquals("SummaryTest", jsonObject.getString("name"));
+        assertTrue(!jsonObject.has("tools"));
+    }
+
+    @Test
+    void testSendRequestSuccess() throws IOException {
+    
+        String jsonRequest = summary.buildJsonForAssistant();
+        String url = "https://api.openai.com/v1/assistants";
+        CompletableFuture<Response> response = summary.sendRequest(jsonRequest, url);
+
+        assertNotNull(response);
+    }
+
+    @Test
+    void testExtractId() throws IOException {
+        Response response = new Response.Builder()
+                .request(new Request.Builder().url("http://url.com").build())
+                .protocol(Protocol.HTTP_1_1)
+                .code(200).message("").body(
+                        ResponseBody.create(
+                                "{\"id\":\"assistant-id-123\"}",
+                                MediaType.parse("application/json")))
+                .build();
+
+        String id = summary.extractId(response);
+
+        assertEquals("assistant-id-123", id);
+    }
+
+    @AfterAll
+    void deleteAssistant() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/assistants/" + summary.getAssistantId())
+                .delete()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer " + summary.loadKey())
+                .addHeader("OpenAI-Beta", "assistants=v2")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful())
+            System.out.println("The delete of assistant is unable");
+    }
+}
+
